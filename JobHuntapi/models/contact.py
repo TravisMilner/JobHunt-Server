9f1655feedc420d27c90d662bb364e61799b5114
Travis Milner <travismilner1@gmail.com>
2021-03-12 13:46:12 -0600

    had to refactor my models and fixtures to reflect user token instead of userId. Completed full CRUD functionality for jobs

--- JobHuntapi/models/contact.py
+++ JobHuntapi/models/contact.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 class Contact(models.Model):
     name = models.CharField(max_length=255)
@@ -7,4 +7,4 @@ class Contact(models.Model):
     email = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
     notes = models.CharField(max_length=300)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Token, on_delete=models.CASCADE)
